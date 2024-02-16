c98daa6d668c81e44c7493a3f09f29b3fab2d5b1
Alex Rio <ar02220604@gmail.com>
2024-02-16 16:13:00 +0900

    features: add-jwt-middleware

--- advertising_matching_platform/middlewares/auth.py
+++ advertising_matching_platform/middlewares/auth.py
@@ -1,28 +1,45 @@
-from django.shortcuts import redirect
-from django.urls import reverse
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
+from django.http import JsonResponse
+
 
 class AuthMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Check if the current path is a public page
-        print(request.path)
-        if request.path in ['/api/auth/login', '/api/auth/register']:
-            # Allow access to public pages
+        # Check if the current path is a public api
+        if request.path in ["/api/auth/login", "/api/auth/register"]:
+            # Allow access to public api
             return self.get_response(request)
+
+        # Extract the access token from the Authorization header
+        access_token = request.META.get("HTTP_AUTHORIZATION", "").split(" ")[1]
+
+        # Use JWTAuthentication to decode and validate the access token
+        jwt_authentication = JWTAuthentication()
+
+        try:
+            validated_token = jwt_authentication.get_validated_token(access_token)
+        except InvalidToken:
+            # Handle invalid token case if needed
+            return JsonResponse({"error": "Invalid access token"}, status=401)
+
+        # Retrieve the user and token objects from the validated token
+        user = jwt_authentication.get_user(validated_token)
+
+        # You can access the authenticated user if necessary
+        request.user = user
 
         # Check if the user is authenticated
         if not request.user.is_authenticated:
-            # Redirect to the login page
-            return redirect(reverse('login'))
+            return JsonResponse({"error": "Unauthorized"}, status=401)
 
         # Check if the current path is an admin page
-        if request.path.startswith('/api/admin/'):
+        if request.path.startswith("/api/admin/"):
             # Check if the user has admin authority
             if not request.user.is_superuser:
-                # Redirect to an unauthorized page or display an error message
-                return redirect(reverse('unauthorized'))
+                return JsonResponse({"error": "Unauthorized"}, status=403)
 
-        # Allow access to private and authorized pages
+        # Allow access to private and authorized api
         return self.get_response(request)
