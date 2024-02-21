81bfcdeeec9ef57c28ad1267c41213009f40fcb0
foryou929 <zxcvwert3457@gmail.com>
2024-02-21 23:03:48 +0900

    features: add-login-with-token

--- auth/serializer.py
+++ auth/serializer.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("username", "email", "password")
+        fields = '__all__'
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
