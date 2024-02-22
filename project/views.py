dbb122ea176eed4694a3cae9764047eea54f7528
Alex Rio <ar02220604@gmail.com>
2024-02-22 22:50:59 +0900

    features: fix-project-list-api

--- project/views.py
+++ project/views.py
@@ -1,3 +1,4 @@
+from django.views import View
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from project.models import List
@@ -16,54 +17,17 @@ class ClientProjectView(CreateAPIView):
         return super().create(request, *args, **kwargs)
 
     def get(self, request):
-        recruiting = ListSerializer(
-            List.objects.filter(user_id=request.user.id, progress=1), many=True
-        )
-        progressing = ListSerializer(
-            List.objects.filter(
-                user_id=request.user.id, progress__gt=1, progress__lt=11
-            ),
-            many=True,
-        )
-        completed = ListSerializer(
-            List.objects.filter(user_id=request.user.id, progress=11), many=True
-        )
-        return Response(
-            {
-                "recruiting": recruiting.data,
-                "progressing": progressing.data,
-                "finish": completed.data,
-            }
-        )
+        queryset = List.objects.filter(user_id=request.user.id)
+        serializer = ListSerializer(queryset, many=True)
+        return Response(serializer)
 
 
-class UserProjectView(CreateAPIView):
+class UserProjectView(View):
     permission_classes = (IsAuthenticated,)
     queryset = List.objects.all()
     serializer_class = ListSerializer
 
-    def create(self, request, *args, **kwargs):
-        # Add foreign key values to request data
-        request.data["user"] = request.user.id
-        return super().create(request, *args, **kwargs)
-
     def get(self, request):
-        recruiting = ListSerializer(
-            List.objects.filter(user_id=request.user.id, progress=1), many=True
-        )
-        progressing = ListSerializer(
-            List.objects.filter(
-                user_id=request.user.id, progress__gt=1, progress__lt=11
-            ),
-            many=True,
-        )
-        completed = ListSerializer(
-            List.objects.filter(user_id=request.user.id, progress=11), many=True
-        )
-        return Response(
-            {
-                "recruiting": recruiting.data,
-                "progressing": progressing.data,
-                "finish": completed.data,
-            }
-        )
+        queryset = List.objects.filter(user_id=request.user.id)
+        serializer = ListSerializer(queryset, many=True)
+        return Response(serializer)
