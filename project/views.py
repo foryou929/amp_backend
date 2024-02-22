4fd197e9ab3f1e15c79e7df7a41575abee908484
foryou929 <zxcvwert3457@gmail.com>
2024-02-22 22:30:40 +0900

    features: add-user-project-manage-api

--- project/views.py
+++ project/views.py
@@ -5,8 +5,39 @@ from project.serializer import ListSerializer
 from rest_framework.response import Response
 
 
-# Create your views here.
-class ProjectView(CreateAPIView):
+class ClientProjectView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Add foreign key values to request data
+        request.data["user"] = request.user.id
+        return super().create(request, *args, **kwargs)
+
+    def get(self, request):
+        recruiting = ListSerializer(
+            List.objects.filter(user_id=request.user.id, progress=1), many=True
+        )
+        progressing = ListSerializer(
+            List.objects.filter(
+                user_id=request.user.id, progress__gt=1, progress__lt=11
+            ),
+            many=True,
+        )
+        completed = ListSerializer(
+            List.objects.filter(user_id=request.user.id, progress=11), many=True
+        )
+        return Response(
+            {
+                "recruiting": recruiting.data,
+                "progressing": progressing.data,
+                "finish": completed.data,
+            }
+        )
+
+
+class UserProjectView(CreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = List.objects.all()
     serializer_class = ListSerializer
