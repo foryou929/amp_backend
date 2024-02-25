51b8a05022cd6961e584f3269ce59105a8ef9565
foryou929 <zxcvwert3457@gmail.com>
2024-02-26 08:52:25 +0900

    features: fix-message-model

--- message/models.py
+++ message/models.py
@@ -3,18 +3,15 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
-from project.models import List as Project
+from section.models import List as Section
 
 
 class List(models.Model):
     sender = models.ForeignKey(
         User, related_name="message_sender", on_delete=models.CASCADE
     )
-    receiver = models.ForeignKey(
-        User, related_name="message_receiver", on_delete=models.CASCADE
-    )
-    project = models.ForeignKey(
-        Project, related_name="message_project", on_delete=models.CASCADE
+    section = models.ForeignKey(
+        Section, related_name="message_section", on_delete=models.CASCADE
     )
     type = models.IntegerField()
     status = models.IntegerField()
