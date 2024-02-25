9e243c2efe5ab87e4cca9b30c973ffb8742bac6f
foryou929 <zxcvwert3457@gmail.com>
2024-02-25 22:47:20 +0900

    features: add-section-user-api

--- section/views.py
+++ section/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+from section.models import List
+from section.serializer import ListSerializer
 
-# Create your views here.
+class MessageView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    def create(self, request, *args, **kwargs):
+        # Add foreign key values to request data
+        return super().create(request, *args, **kwargs)
+
+
+class ClientView(ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    # def get_queryset(self):
+    #     queryset = List.objects.filter()
+    #     return 
+
+
+class UserView(RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    lookup_field = "project_id"
