949ea021abb8302981dd0e3c093a8b55ce17ea55
Alex Rio <ar02220604@gmail.com>
2024-02-26 15:08:39 +0900

    features: add-project-scout-view-api

--- project/serializer.py
+++ project/serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from project.models import List
+from section.serializer import ListSerializer as SectionSerializer
 
 
 class ListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = List
+        fields = "__all__"
+
+
+class ProjectSectionSerializer(serializers.ModelSerializer):
+    sections = SectionSerializer(many=True, read_only=True)
+
     class Meta:
         model = List
         fields = "__all__"
