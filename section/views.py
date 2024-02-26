47deb5aafc7c200b0116937d3db8978e22c496ae
foryou929 <zxcvwert3457@gmail.com>
2024-02-26 17:33:21 +0900

    features: add-client-section-update-api

--- section/views.py
+++ section/views.py
@@ -1,23 +1,27 @@
 from django.http import Http404
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
 from rest_framework.permissions import IsAuthenticated
 from section.models import List
 from section.serializer import ListSerializer
 
 
-class SectionView(CreateAPIView):
+class SectionView(RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = List.objects.all()
     serializer_class = ListSerializer
-
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+    lookup_field = "id"
 
 
-class ClientSectionView(ListAPIView):
+class ClientSectionView(ListAPIView, UpdateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    lookup_field = "id"
     # def get_queryset(self):
     #     queryset = List.objects.filter()
     #     return
@@ -39,7 +43,7 @@ class UserSectionView(RetrieveAPIView, CreateAPIView):
         queryset = self.get_queryset()
         fiter = {"user": self.request.user.id}
         obj = queryset.filter(**fiter).first()
-    
+
         if obj is None:
             raise Http404
 
