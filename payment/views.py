aa1daca504443710a9cfbaf15eee2188f1325468
Alex Rio <ar02220604@gmail.com>
2024-02-27 15:33:45 +0900

    features: add-payment-api

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from payment.models import List
+from payment.serializer import ListSerializer
+
 
 # Create your views here.
+class PaymentView(CreateAPIView, RetrieveAPIView, UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    lookup_field = "section_id"
+
+    def create(self, request, *args, **kwargs):
+        request.data["section"] = self.kwargs.get("section_id")
+        return super().create(request, *args, **kwargs)
