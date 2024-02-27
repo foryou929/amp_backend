b1cd98036136dc1fa345ba5c07a1b278f8a6f4a6
Alex Rio <ar02220604@gmail.com>
2024-02-27 15:51:12 +0900

    features: add-section-api

--- section/models.py
+++ section/models.py
@@ -4,11 +4,9 @@ from project.models import List as Project
 
 
 class List(models.Model):
-    project = models.ForeignKey(
-        Project, related_name="sections", on_delete=models.CASCADE
-    )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    step = models.IntegerField(default=1)
+    project = models.ForeignKey(Project, related_name="section_projects", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="section_users", on_delete=models.CASCADE)
+    step = models.IntegerField(default=2)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
