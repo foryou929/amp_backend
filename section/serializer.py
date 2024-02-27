760091a5459edee0eb14c945d6e0778db45227b1
foryou929 <zxcvwert3457@gmail.com>
2024-02-27 15:51:28 +0900

    features: fix-section-api

--- section/serializer.py
+++ section/serializer.py
@@ -4,9 +4,12 @@ from message.serializer import ListSerializer as MessageSerializer
 
 
 class ListSerializer(serializers.ModelSerializer):
+    suggest_count = serializers.IntegerField()
+
     class Meta:
         model = List
         fields = "__all__"
+        depth = 2
 
 
 class SectionMessageSerializer(serializers.ModelSerializer):
