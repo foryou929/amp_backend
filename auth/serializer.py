72a5eeebc8392d8844dbb12012fca029009390ad
foryou929 <zxcvwert3457@gmail.com>
2024-02-29 17:34:53 +0900

    features: integrate-auth&user

--- auth/serializer.py
+++ auth/serializer.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from user.models import List
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = List
         fields = '__all__'
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         password = validated_data.pop("password")
-        user = User(**validated_data)
+        user = List(**validated_data)
         user.set_password(password)
         user.save()
         return user
