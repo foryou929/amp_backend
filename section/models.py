f77fde291936a3fe726dba92f3a669e21bdb9635
foryou929 <zxcvwert3457@gmail.com>
2024-02-29 17:35:59 +0900

    features: fix-project-api

--- section/models.py
+++ section/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from project.models import List as Project
 
 
 class List(models.Model):
     project = models.ForeignKey(Project, related_name="project_sections", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name="sections", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="sections", on_delete=models.CASCADE)
     step = models.IntegerField(default=1)
 
     created_at = models.DateTimeField(auto_now_add=True)
