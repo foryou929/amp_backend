ee9f3b5c0aaa6df4a86626162da73bed2e24d64b
foryou929 <zxcvwert3457@gmail.com>
2024-03-02 02:14:59 +0900

    features: fix-asgi-auth-middleware

--- server/asgi.py
+++ server/asgi.py
@@ -1,19 +1,11 @@
-"""
-ASGI config for server project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from ws.middleware import TokenAuthMiddleware
 from ws.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": URLRouter(websocket_urlpatterns),
+        "websocket": TokenAuthMiddleware(URLRouter(websocket_urlpatterns)),
     }
 )
