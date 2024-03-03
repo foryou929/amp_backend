4b1b26e21dd67ee8934b387148c867cd91565d11
Alex Rio <ar02220604@gmail.com>
2024-03-03 21:47:37 +0900

    features: remove-uneccessary-commet

--- message/views.py
+++ message/views.py
@@ -14,21 +14,3 @@ class MessageView(ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         request.data["section"] = self.kwargs.get("section_id")
         return super().create(request, *args, **kwargs)
-
-
-# class ClientMessageView(ListAPIView):
-#     permission_classes = (IsAuthenticated,)
-#     queryset = List.objects.all()
-#     serializer_class = ListSerializer
-# def get_queryset(self):
-#     queryset = List.objects.filter()
-#     return
-
-
-# class UserMessageView(ListAPIView):
-#     permission_classes = (IsAuthenticated,)
-#     queryset = List.objects.all()
-#     serializer_class = ListSerializer
-# def get_queryset(self):
-#     section_id = self.kwargs.get('section_id')
-#     return List.objects.filter(sender_id=self.request.user, section_id=section_id)
