dcd6c3d0c916d3939e2aed57cf28a34806ab3159
Alex Rio <ar02220604@gmail.com>
2024-03-04 22:15:14 +0900

    features: change-model-file-field-to-source

--- file/views.py
+++ file/views.py
@@ -1,5 +1,4 @@
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from file.models import List
