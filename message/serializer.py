48243b3472c18096df1bf4092f6be6f77afa44a5
Alex Rio <ar02220604@gmail.com>
2024-03-04 14:44:41 +0900

    features: add-message-file-upload-api

--- message/serializer.py
+++ message/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from message.models import List
+from file.serializer import Serializer as FileSerializer
 
 
 class Serializer(serializers.ModelSerializer):
@@ -9,6 +10,8 @@ class Serializer(serializers.ModelSerializer):
 
 
 class ReadSerializer(serializers.ModelSerializer):
+    message_files = FileSerializer(many=True, read_only=True)
+
     class Meta:
         model = List
         fields = "__all__"
