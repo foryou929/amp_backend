334500c4a418ad0b42ab48f939524f48d0b44083
Alex Rio <ar02220604@gmail.com>
2024-03-04 10:14:35 +0900

    features: add-message-create-api

--- message/serializer.py
+++ message/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from message.models import List
 
 
-class ListSerializer(serializers.ModelSerializer):
+class Serializer(serializers.ModelSerializer):
     class Meta:
         model = List
         fields = "__all__"
