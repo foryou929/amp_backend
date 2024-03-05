fbc851dc3246ad5adce989e7d3e4f6537066ab59
foryou929 <zxcvwert3457@gmail.com>
2024-03-05 11:09:56 +0900

    features: fix-section-project-image-list-bug

--- section/serializer.py
+++ section/serializer.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from section.models import List
+from project.models import List as ProjectList
+from image.models import List as ImageList
 from message.serializer import Serializer as MessageSerializer
+from datetime import datetime
 
 
 class Serializer(serializers.ModelSerializer):
@@ -16,11 +19,29 @@ class LinkSerializer(serializers.ModelSerializer):
         depth = 1
 
 
-class ReadSerializer(serializers.ModelSerializer):
+class ImageListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImageList
+        fields = "__all__"
+
+
+class ProjectListSerializer(serializers.ModelSerializer):
+    project_images = ImageListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ProjectList
+        fields = "__all__"
+
+
+class SectionListSerializer(serializers.ModelSerializer):
+    project = ProjectListSerializer(read_only=True)
     section_messages = MessageSerializer(many=True, read_only=True)
     suggest_count = serializers.IntegerField()
+    current = serializers.SerializerMethodField()
 
     class Meta:
         model = List
         fields = "__all__"
-        depth = 1
+
+    def get_current(self, obj):
+        return datetime.now()
