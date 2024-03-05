6effde3f468172c144d693b172e6c5bb4232c47a
Alex Rio <ar02220604@gmail.com>
2024-03-05 11:17:53 +0900

    features: add-space-view-image

--- space/views.py
+++ space/views.py
@@ -1,15 +1,30 @@
-from rest_framework.generics import CreateAPIView
+from utils.views import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from space.models import List
-from space.serializer import ListSerializer
+from space.serializer import Serializer, ReadSerializer
 
 
 # Create your views here.
-class SpaceView(CreateAPIView):
+class SpacesView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = List.objects.all()
-    serializer_class = ListSerializer
+    serializer_class = Serializer
+    read_serializer_class = ReadSerializer
 
     def create(self, request, *args, **kwargs):
-        request.data["user"] = request.user.id
+        request.data["creator"] = request.user.id
         return super().create(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.kwargs.get("type") == "user":
+            queryset = queryset.filter(creator=self.request.user.id)
+        return queryset
+
+
+class SpaceView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = List.objects.all()
+    serializer_class = Serializer
+    read_serializer_class = ReadSerializer
+    lookup_field = "id"
