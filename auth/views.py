303f1c973f15d89068a2f2aa7711c195dfca218d
foryou929 <zxcvwert3457@gmail.com>
2024-03-07 23:59:47 +0900

    features: fix-token-encode-decode-api

--- auth/views.py
+++ auth/views.py
@@ -2,19 +2,10 @@ from django.contrib.auth import authenticate, logout
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from auth.serializer import UserSerializer
-
-
-class RegisterView(views.APIView):
-    permission_classes = (AllowAny,)
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Registration successful"}, status=201)
-        return Response(serializer.errors, status=400)
+from server.settings import SECRET_KEY
+import jwt
 
 
 class LoginView(views.APIView):
@@ -27,14 +18,28 @@ class LoginView(views.APIView):
             user = authenticate(request, username=username, password=password)
             if user:
                 refresh = RefreshToken.for_user(user)
+                access = AccessToken.for_user(user)
+                refresh_token = jwt.encode(refresh.payload, SECRET_KEY, algorithm='HS256')
+                access_token = jwt.encode(access.payload, SECRET_KEY, algorithm='HS256')
                 token = {
-                    "refresh": str(refresh),
-                    "access": str(refresh.access_token),
+                    "refresh": refresh_token,
+                    "access": access_token,
                 }
                 return Response({"message": "Login successful", "token": token})
             return Response({"error": "Invalid credentials"}, status=400)
         except Exception as e:
             return Response({"error": str(e)}, status=500)
+        
+
+class RegisterView(views.APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Registration successful"}, status=201)
+        return Response(serializer.errors, status=400)
 
 
 class LoginWithTokenView(views.APIView):
@@ -49,3 +54,10 @@ class LogoutView(views.APIView):
     def post(self, request):
         logout(request)
         return Response({"message": "Logout successful"})
+
+
+class TokenRefreshView(views.APIView):
+    def post(self, request):
+        refresh = RefreshToken(request.data["refresh"])
+        access_token = jwt.encode(refresh.access_token.payload, SECRET_KEY, algorithm='HS256')
+        return Response({"access": access_token})
