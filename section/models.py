88b6c522ae9a0d7f2bfbbe8fdbcdf056681cf83b
foryou929 <zxcvwert3457@gmail.com>
2024-03-09 01:55:52 +0900

    features: add-section-model-space-fk

--- section/models.py
+++ section/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
 from project.models import List as Project
+from space.models import List as Space
 
 
 class List(models.Model):
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name="project_sections")
+    space = models.ForeignKey(Space, on_delete=models.SET_NULL, null=True, related_name="space_sections")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="sections")
     step = models.IntegerField(default=1)
 
